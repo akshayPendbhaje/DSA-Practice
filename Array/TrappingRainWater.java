ede59a89273b3bba5f372f0e1fe312c482925802
Akshay Pendbhaje <akshay.pendbhaje@gmail.com>
2022-02-17 06:32:04 +0530

    trapping rqainwater

--- Array/TrappingRainWater.java
+++ Array/TrappingRainWater.java
@@ -40,3 +40,46 @@ public class TrappingRainWater {
  **************** Logic **************** Trapped water at any index = min(max height left, max
  * height right) - current height
  */
+
+ /* OR can be done with two pointer approach */
+
+
+ class Solution {
+    public int trap(int[] height) {
+        
+        int leftMax=height[0];
+        int rightMax=0;
+        int left=0;
+        int right=height.length-1;
+        int ans=0;
+        
+        while(left<right){
+            if(height[left]<height[right])
+            {
+                if(height[left]>=leftMax)
+                {
+                    leftMax=height[left];
+                }
+                else
+                {
+                    ans+=leftMax-height[left];
+                }
+                left++;
+                
+            }
+            else
+            {
+                if(height[right]>=rightMax)
+                {
+                    rightMax=height[right];
+                }
+                else
+                {
+                    ans+=rightMax-height[right];
+                }
+                right--;
+            }
+        }
+        return ans;
+    }
+}
