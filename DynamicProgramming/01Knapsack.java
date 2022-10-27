b5bfe8e04a4cd23255f8410c511a28150aa48eda
Akshay Pendbhaje <akshay.pendbhaje@somaiya.edu>
2022-10-27 22:39:19 +0530

    knapsack top down approach

--- DynamicProgramming/01Knapsack.java
+++ DynamicProgramming/01Knapsack.java
@@ -50,6 +50,36 @@ class Solution
 
 }
 
+///Top down approach
+
+
+class Solution
+{
+    public static int[][] dp;
+    //Function to return max value that can be put in knapsack of capacity W.
+    static int knapSack(int W, int wt[], int val[], int n)
+    {
+        dp =  new int[n+1][W+1];
+
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=W;j++)
+            {
+                if(wt[i-1]<=j)
+                    dp[i][j]=Math.max(val[i-1]+dp[i-1][j-wt[i-1]],dp[i-1][j]);
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+
+        return dp[n][W];
+    }
+
+
+}
+
+
+
 
 /*
 **************** Logic ****************
